9ffaa656c490cb3fab6131498d15d6a910350bd1
Mariusz Zakrzewski <fullstack_developer@MacBook-Pro-Mariusz.local>
2026-08-01 21:58:29 +0200

    Initial commit

---
+++ ConsoleLearnCSharp/Math.cs
@@ -0,0 +1,14 @@
+namespace ConsoleLearnCSharp;
+
+public class Math
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+}
---
+++ ConsoleLearnCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleLearnCSharp
+{
+    public static class Program
+    {
+        public static void Main(string[] arg)
+        {
+            double a = 20;
+            double b = 10;
+
+            Math math = new Math();
+
+            double addResults = math.Add(a, b);
+            double subtractResults = math.Subtract(a, b);
+            
+            Console.WriteLine($"AddResults: {addResults}");
+            Console.WriteLine($"SubtractResults: {subtractResults}");
+        }
+    }
+}
+
